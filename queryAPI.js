94e517298adcda939fa42d1d8b458cd0fad4ec72
JaagupLasn <jaaguplasn@outlook.com>
2022-12-29 11:59:14 +0200

    test

--- queryAPI.js
+++ queryAPI.js
@@ -14,6 +14,6 @@ export async function queryAPI(query, variables = {}) {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      console.log(data[data]);
     });
 }
