1372872fd4b3008c80dd6277e747ea1905e40f55
JaagupLasn <jaaguplasn@outlook.com>
2022-12-29 13:16:16 +0200

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ let info = {
   level: null,
   xp: null,
 };
-let res = await queryAPI(LoginQuery, { login: "jaaguplasn" });
+export async function start(logininfo) {
+let res = await queryAPI(LoginQuery, { login: logininfo });
 info.id = res.data.user[0].id;
 res = await queryAPI(LevelQuery, { id: info.id });
 info.level = res.data.transaction[0].amount;
 console.log("info is: ", info);
+}
