24b70082fe25ec4cfbb829a7d6da7f37755c06c7
JaagupLasn <jaaguplasn@outlook.com>
2022-12-29 12:37:28 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ import { queryAPI } from "./queryAPI.js";
 import { LoginQuery, LevelQuery } from "./queries.js";
 
 let info = {
-    id: null,
-    level: null,
-    xp: null
-}
-let res = await queryAPI(LoginQuery, {login: "jaaguplasn"});
-info.id = res.data.user[0].id
-res = await queryAPI(LevelQuery, {id: info.id});
-console.log(res.data.transaction[0].amount)
-//info.level = res.data.transaction[0].amount
-//console.log("info is: ", info)
+  id: null,
+  level: null,
+  xp: null,
+};
+let res = await queryAPI(LoginQuery, { login: "jaaguplasn" });
+info.id = res.data.user[0].id;
+res = await queryAPI(LevelQuery, { id: info.id });
+info.level = res.data.transaction[0].amount;
+console.log("info is: ", info);
