2ddd011fca7817395e175fa0ab8d847873982725
JaagupLasn <jaaguplasn@outlook.com>
2022-12-29 12:36:23 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -10,5 +10,5 @@ let res = await queryAPI(LoginQuery, {login: "jaaguplasn"});
 info.id = res.data.user[0].id
 res = await queryAPI(LevelQuery, {id: info.id});
 console.log(res.data.transaction[0].amount)
-info.level = res.data.transaction[0].amount
-console.log("info is: ", info)
+//info.level = res.data.transaction[0].amount
+//console.log("info is: ", info)
