761ba005b8c679714f728d1361bc4a5f04eaf579
JaagupLasn <jaaguplasn@outlook.com>
2022-12-29 12:05:09 +0200

    modified:   queryAPI.js

--- queryAPI.js
+++ queryAPI.js
@@ -1,19 +1,20 @@
 export async function queryAPI(query, variables = {}) {
   try {
     let response = fetch("https://01.kood.tech/api/graphql-engine/v1/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      query,
-      variables
-    }),
-  })
-  if (!response.ok) throw Error("Couldn't fetch data from API")
-  let data = await response.json()
-} catch (e) {
-  alert(e)
-}
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    });
+    console.log(response);
+    if (!response.ok) throw Error("Couldn't fetch data from API");
+    let data = await response.json();
+  } catch (e) {
+    alert(e);
+  }
 }
