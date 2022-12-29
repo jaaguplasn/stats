5734fde3607fcef6b32677fe5f996b1ae6a87174
JaagupLasn <jaaguplasn@outlook.com>
2022-12-29 12:09:15 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import { queryAPI } from "./queryAPI.js";
 import { query, LevelQuery } from "./queries.js";
+
+let info = {
+    id: null,
+    level: null,
+    xp: null
+}
 let data = await queryAPI(LevelQuery, {id: "541"});
-console.log(data)
+console.log(data[data])
