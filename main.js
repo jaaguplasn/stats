cc0d34cd3dbc49720c891736b931486fe15de58e
JaagupLasn <jaaguplasn@outlook.com>
2022-12-30 16:58:54 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 import { queryAPI } from "./queryAPI.js";
 import { LoginQuery, LevelQuery, TaskQuery } from "./queries.js";
 
+class Task {
+  constructor(name, date, xp) {
+    (this.name = name), (this.date = date), (this.xp = xp);
+  }
+}
+
 let info = {
   id: null,
   level: null,
   xp: null,
   tasks: { Task },
 };
-class Task {
-  constructor(name, date, xp) {
-    (this.name = name), (this.date = date), (this.xp = xp);
-  }
-}
 
 let res = await queryAPI(LoginQuery, { login: "jaaguplasn" });
 info.id = res.data.user[0].id;
@@ -22,8 +23,7 @@ console.log(res.data.progress);
 for (let i = 0; i < res.data.progress.length; i++) {
   //get task xp
   let xpData = await queryAPI(TaskQuery, { login: "jaaguplasn" });
-  console.log(xpData)
+  console.log(xpData);
   //task.name = res.data.progress[i].object.name;
   //task.date = res.data.progress[i].updatedAt
 }
-
