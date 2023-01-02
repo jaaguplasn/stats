92be4444914ad581ae389e13266c13f82555b287
JaagupLasn <jaaguplasn@outlook.com>
2023-01-02 08:41:37 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import { LoginQuery } from "./queries.js"
 
 const User = "jaaguplasn"
 
+
+//magic
 const btn = document.querySelector('.search-btn')
-.addEventListener("click", async(event) => {
+.addEventListener("click", async(event) => { 
   event.preventDefault()
   let username = document.getElementById("searchInput").value
   let userId = await LoginQuery(username); //only for seeing if user exists
@@ -14,6 +16,12 @@ const btn = document.querySelector('.search-btn')
     console.log("username and id are: ", username, " ", userId)
     let info = await getInfo(username, userId.id)
     console.log(info)
+    //insert magic here
   }
 })
 
+//on page load start magic with default user
+window.addEventListener("Load", function(_) {
+  document.querySelector("#searchInput").value = User
+  document.querySelector(".search-btn").click()
+})
