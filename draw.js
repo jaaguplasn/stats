0c3b1b73df88019643789d9c1bb13f95ee14c22b
JaagupLasn <jaaguplasn@outlook.com>
2023-01-04 16:13:45 +0200

    modified:   draw.js 	modified:   index.html

--- draw.js
+++ draw.js
@@ -1,6 +1,18 @@
 import { getInfo } from "./info.js";
 
 export async function Draw(username, id) {
-  let userData = await getInfo(username, id);
-  console.log(userData);
+  let UserData = await getInfo(username, id);
+  console.log(UserData);
+  let InfoBoxes = document.getElementById("infoboxes");
+  let UserInfoBox = document.createElement("div");
+  UserInfoBox.innerText =
+    "Username: " +
+    UserData.username +
+    "\n" +
+    "ID: " +
+    UserData.id +
+    "\n" +
+    "Level: " +
+    UserData.level;
+  InfoBoxes.appendChild(UserInfoBox);
 }
